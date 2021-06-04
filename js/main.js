8b1b881a5b338c0aba7eb7dbcb9bb25e3fc612ca
Vadym_Novaiev1992 <81385681+Novaiev1992@users.noreply.github.com>
2021-06-04 19:15:24 +0300

    My files

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function(){
+    let burgerItem = document.querySelector('.header-burger');
+   let menu = document.querySelector('.header-links-bg-fixed-mobile');
+   let menuCloseItem = document.querySelector(".header-nav-close-mobile");
+   burgerItem.addEventListener('click', () => {
+       menu.classList.add('header-links-bg-fixed-mobile-active');
+       document.body.classList.add('overflow')
+   })
+   menuCloseItem.addEventListener('click', () =>{
+       menu.classList.remove('header-links-bg-fixed-mobile-active');
+       document.body.classList.remove('overflow')
+   })
+
+}())
